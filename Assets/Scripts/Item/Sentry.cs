9a7e7a7759e645557e6416c503439966a829a050
Xwilarg <xwilarg@yahoo.fr>
2021-03-22 22:10:26 +0100

    Fix missing sprites

--- Assets/Scripts/Item/Sentry.cs
+++ Assets/Scripts/Item/Sentry.cs
@@ -1,9 +1,15 @@
 ﻿using UnityEngine;
+using UnityEngine.Events;
 
 namespace Gashapon.Item
 {
-    public class PlayerController : MonoBehaviour
+    public class Sentry : MonoBehaviour
     {
+        public void Start()
+        {
+            GetComponent<Interractible>().SetCallback(new UnityAction(Fire));
+        }
+
         private void Fire()
         {
             Debug.Log("Firing");
